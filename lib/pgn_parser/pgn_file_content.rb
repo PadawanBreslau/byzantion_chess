337abfc6bff8d56f20d9c9d8734202a46fd2db01
Stanislaw Zawadzki <st.zawadzki@gmail.com>
2014-08-09 09:15:31 +0200

    Parser fix - whitespaces

--- lib/pgn_parser/pgn_file_content.rb
+++ lib/pgn_parser/pgn_file_content.rb
@@ -3,11 +3,13 @@ require 'byzantion_chess'
 class PgnFileContent
   attr_accessor :content
 
-  def initialize(content)
+  def initialize(content, logger=nil)
     @content = content
+    @logger = logger
   end
 
   def parse_games
+    @logger.info @content if @logger
     parsed_file = Parser.parse(@content)
     parsed_file.map do |one_game|
       game = Game.new(one_game)
